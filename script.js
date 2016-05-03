61d6988f3d42415ec6eb2ef25bebd24563664f58
Pol-Lanski <pol.bordas.baches@gmail.com>
2016-05-03 22:21:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi / Hola! \n escriu CAT per parlar en català, ESP para hablar en castellano and ENG to speak in english.')
+            return bot.say('Hi / Hola! \n Escriu CAT per parlar en català. \n Escribe ESP para hablar en castellano \n Type ENG to speak in English.')
                 .then(() => 'speak');
         }
     },
